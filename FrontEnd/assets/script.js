1a8188d67b84fabf87dcf4e6be33dae250326e20
Wilfried Urban <wilfriedu@gmail.com>
2023-06-22 12:11:21 +0200

    HS

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:5678/api/works")
+/*fetch("http://localhost:5678/api/works")
 .then(reponce=>{
     return reponce.json()
 })
@@ -21,3 +21,48 @@ function creerWok(work){
     let divGallery=document.querySelector(".gallery")
     divGallery.appendChild(figur)
 }
+*/
+const works = fetch("http://localhost:5678/api/works").then(response => response.json())
+
+const buttonObjects = document.querySelector(".Objects")
+const buttonApartements = document.querySelector(".Apartements")
+const buttonHotels = document.querySelector(".Hotels")
+const buttonAll = document.querySelector(".All")
+
+
+buttonObjects.addEventListener("click", function () {
+    const filterObjects = works.filter(work => work.category.name === "Objets")
+    
+    console.log(filterObjects)
+})
+
+const fetchWorks = async () => {
+    const response = await fetch("http://localhost:5678/api/works")
+
+    return response.json()
+}
+
+const init = async () => {
+    const works2 = await fetchWorks()
+    renderWorks(works2)
+    console.log({works2})
+}
+
+init()
+
+
+
+const renderWorks = (works) => {
+    let template= ''
+      works.forEach(work => {
+        template += `
+        <figure>
+             <img alt="${work.title}" src="${work.imageUrl}"/>
+        <figcaption>${work.title}</figcaption>
+        </figure>
+             `
+       } )
+    
+      document.getElementById('gallery').innerHTML = template
+      
+    }
