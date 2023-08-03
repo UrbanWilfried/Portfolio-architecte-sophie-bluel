c9bf6b41fba881962ef8a2351fae43da4ea17046
urbanwilfried <wilfriedu@gmail.com>
2023-08-03 17:48:29 +0200

    ajout boite dialog html

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,42 +1,27 @@
 let loginForm = document.getElementById("loginForm");
+const loginError = document.querySelector('.error');
+
+function redirectionHomePage(){
+  document.location.href="index.html"; 
+}
 
 loginForm.addEventListener("submit", async (e) => {
-  
+
   e.preventDefault();
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
-  const loginError = document.querySelectorAll('.error');
-  const bannerEdit = document.querySelectorAll('.bannerEdit')
-  const editImage = document.querySelectorAll('.editImage')
-  const editTitre = document.querySelectorAll('.editTitre')
+  
   console.log(emailValue, passwordValue)
   if (emailValue && passwordValue) {
-    try {
       const response = await login(emailValue, passwordValue)
-      console.log(response)
-      function RedirectionJavascript(){
-        document.location.href="index.html"; 
+      console.log("cototo", response)
+      if (response.token){
+        localStorage.setItem("token", response.token)
+        redirectionHomePage()
+      } else{
+        loginError.classList.add('toto')
+        console.log("tyuio", error)
       }
-      bannerEdit.classList.add('vu')
-      editImage.classList.add('vu2')
-      editTitre.classList.add('vu3')
-    } catch (error) {
-      loginError.classList.add('toto')
-      /*throw new Error("Information non valide");*/
-      /*document.getElementById('error').innerHTML="Information non valide";*/
-      /*function validateForm()                                 
-         { 
-             var name = document.forms["email"]["password"];         
-             if (name.value == ""){ 
-                 document.getElementById('error').innerHTML="Information non valide";  
-                 name.focus(); 
-                 return false; 
-             }else{
-                 document.getElementById('error').innerHTML="";  
-             }
-         }*/
-      console.log(error)
-    }
   }
 });
 const login = async (email, password) => {
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,12 +1,24 @@
 import createGallery from "./gallery.js";
 import createFilters from "./filters.js";
 
+const bannerEdit = document.querySelector('.bannerEdit')
+const editImage = document.querySelector('.editImage')
+const editProject = document.querySelector('.editProject')
+const filterNone = document.querySelector('filter')
+
 const fetchWorks = async () => {
     const response = await fetch("http://localhost:5678/api/works")
     return response.json()
 };
 
 const init = async () => {
+  const token=localStorage.getItem("token")
+  if (token) {
+    bannerEdit.style.display='flex'
+    editImage.style.visibility='visible'
+    editProject.style.display='flex'
+    /*filterNone.style.visibility='hidden'*/
+  }
   const worksData = await fetchWorks();
   const gallery = createGallery({ works: worksData });
   gallery.renderGallery();
