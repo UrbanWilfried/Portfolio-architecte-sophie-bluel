be2a8e6fd3fe919c0da86bf29eed547a584b3fd7
urbanwilfried <wilfriedu@gmail.com>
2023-08-17 17:52:22 +0200

    supprimer enregistrer img html js

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -1,10 +1,75 @@
 const closeModal = document.querySelector(".exitModal");
 const closeModal2 = document.querySelector(".exitModal2");
+const ajouPhoto = document.querySelector(".ajouPhoto");
+const section1 = document.querySelector(".section1");
+const section2 = document.querySelector(".section2");
+const previous = document.querySelector(".previous");
+const showDialog = document.querySelector("#showDialog");
+const imgLaod = document.getElementById("imgLaod");
+const previewFile = document.getElementById("preview-file");
 
-const createModal = ({ gallery }) => {
+showDialog.addEventListener('click', () => {
+  section1.style.display='block'
+});
+
+ajouPhoto.addEventListener('click', () => {
+  section1.style.display='none'
+  section2.style.display='block'
+});
+
+previous.addEventListener('click', () => {
+  section1.style.display='block'
+  section2.style.display='none'
+  framAddPhotoI.style.display='block'
+  importImg.style.display='block'
+  framAddPhotoP.style.display='block'
+  previewFile.style.display='none'
+  resetForm();
+});
+
+closeModal.addEventListener('click', () => {
+  section1.style='none'
+});
+
+closeModal2.addEventListener('click', () => {
+  section1.style='none'
+  section2.style.display='none'
+  framAddPhotoI.style.display='block'
+  importImg.style.display='block'
+  framAddPhotoP.style.display='block'
+  previewFile.style.display='none'
+  resetForm()
+});
+
+imgLaod.addEventListener('click', () => {
+  previewFile.style.display='block'
+});
+
+const form = document.getElementById("addWorkForm");
+
+const handleFormSubmit = () => {
+  const image = form.querySelector("input[type='file']").files[0];
+  const title = form.querySelector("input[type='text']").value;
+  const categoryId = form.querySelector("select").value;
+
+  const formData = new FormData();
+  formData.append("image", image);
+  formData.append("title", title);
+  formData.append("category", categoryId);
+
+  return formData;
+};
+
+const createModal = ({ gallery, onSave }) => {
   const dialogElement = document.getElementById("dialog");
   const galleryModalElement = document.querySelector(".gallery-modal");
   
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = handleFormSubmit();
+
+    onSave(formData);
+  });
 
   const updateModal = (newGallery) => {
     gallery.setGallery(newGallery);
@@ -46,47 +111,88 @@ const createModal = ({ gallery }) => {
 
 export default createModal;
 
-const ajouPhoto = document.querySelector('.ajouPhoto');
-const section1 = document.querySelector('.section1');
-const section2 = document.querySelector('.section2');
-const previous = document.querySelector('.previous');
-const showDialog = document.querySelector('#showDialog');
+const logout = document.querySelector('.editButton');
 
-showDialog.addEventListener('click', () => {
-  section1.style.display='block'
+logout.addEventListener('click', () => {
+  localStorage.removeItem("token")
+  location.reload()
+  console.log('exit')
 });
 
-ajouPhoto.addEventListener('click', () => {
-  section1.style.display='none'
-  section2.style.display='block'
+const framAddPhotoI = document.querySelector(".framAddPhoto i")
+const importImg = document.querySelector(".importImg")
+const framAddPhotoP = document.querySelector(".framAddPhoto p")
+      
+function createThumbnail(sFile,sId) {
+  const oReader = new FileReader();
+  oReader.addEventListener(
+    "load", 
+    function() {
+      const oImgElement = document.createElement('img');
+      oImgElement.classList.add('imgPreview') 
+      oImgElement.src = this.result;
+      document.getElementById('preview-'+sId).appendChild(oImgElement);
+    }, 
+    false
+  );
+  framAddPhotoI.style.display='none'
+  importImg.style.display='none'
+  framAddPhotoP.style.display='none'
+  oReader.readAsDataURL(sFile);
+}
+
+function changeInputFil(oEvent){
+  let oInputFile = oEvent.currentTarget,
+    sName = oInputFile.name,
+    aFiles = oInputFile.files,
+    aAllowedTypes = ['png', 'jpg'],
+    imgType;  
+  document.getElementById('preview-'+sName).innerHTML ='';
+  for (let i = 0 ; i < aFiles.length ; i++) {
+    imgType = aFiles[i].name.split('.');
+    imgType = imgType[imgType.length - 1];
+    if(aAllowedTypes.indexOf(imgType) != -1) {
+      createThumbnail(aFiles[i],sName);
+    }
+  }
+  document.getElementById('preview-');
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+  const aFileInput = document.forms['addWorkForm'].querySelectorAll('[type=file]');
+  for(let k = 0; k < aFileInput.length;k++){
+    aFileInput[k].addEventListener('change', changeInputFil, false);
+  }
 });
 
 function resetForm() {
-  document.getElementById("imgLaod").value="";
+  document.getElementById("preview-file").value="";
   document.getElementById("titre").value="";
-  document.getElementById("categorie").value="";
-  console.log("ici")
+  document.getElementById("category").value="";
 }
 
-previous.addEventListener('click', () => {
-  section1.style.display='block'
-  section2.style.display='none'
-  resetForm();
-});
 
-closeModal.addEventListener('click', () => {
-  section1.style='none'
-});
+/*
+const titre = document.getElementById("titre");
+const categorie = document.getElementById("categorie");
+const validImg = document.querySelector(".validImg");
 
-closeModal2.addEventListener('click', () => {
-  section1.style='none'
-  section2.style.display='none'
-  resetForm()
-});
+validImg.addEventListener("input", validFormImg);
 
-const logout = document.querySelector('.editButton');
-  logout.addEventListener('click', () => {
-    localStorage.removeItem("token")
-    location.reload()
-    console.log('exit')
-  });
+function validFormImg() {
+  if (imgLaod.value && titre.value && categorie.value) {
+    validImg.removeAttribute("disabled");
+    validImg.classList.add("disabled");
+  }
+};
+*/
+
+
+
+
+
+
+'http://localhost:5678/api/categories'
+
+
+
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -8,12 +8,7 @@ const editProject = document.querySelector(".editProject");
 const filterNone = document.querySelector(".filter");
 const showButton = document.getElementById("showDialog");
 const galleryContainer = document.getElementById("gallery");
-const deleteWorkButton = document.getElementById("delete-work");
 const token = localStorage.getItem("token");
-const imgLaod = document.getElementById("imgLaod");
-const titre = document.getElementById("titre");
-const categorie = document.getElementById("categorie");
-const validImg = document.querySelector(".validImg");
 
 const fetchWorks = async () => {
   const response = await fetch("http://localhost:5678/api/works");
@@ -30,12 +25,20 @@ const deleteWork = async (workId) => {
   });
   return response.json();
 };
-/* ici ça marche pas */
-/*deleteWorkButton.addEventListener("click", (event) => {
-  deleteWork();
-  console.log("delete img")
-});*/
-/*fin*/
+
+const addWork = async (formData) => {
+  const response = await fetch(`http://localhost:5678/api/works`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: formData,
+  });
+  return response.json();
+};
+
+
+
 const init = async () => {
   if (token) {
     bannerEdit.style.display = "flex";
@@ -62,6 +65,14 @@ const init = async () => {
   });
   const modal = createModal({
     gallery: modalGallery,
+    onSave: async (formData) => {
+      console.log("save", formData);
+      try {
+        await addWork(formData);
+      } catch (e) {
+        console.log(e);
+      }
+    },
   });
   createFilters({
     worksData,
@@ -76,54 +87,21 @@ const init = async () => {
 };
 init();
 
-const framAddPhotoI = document.querySelector(".framAddPhoto i")
-const toto = document.querySelector(".toto")
-      
-function createThumbnail(sFile,sId) {
-  var oReader = new FileReader();
-  oReader.addEventListener('load', function() {
-    var oImgElement = document.createElement('img');
-    oImgElement.classList.add('imgPreview') 
-    oImgElement.src = this.result;
-    document.getElementById('preview-'+sId).appendChild(oImgElement);
-  }, false);
-  framAddPhotoI.style.display='none'
-  oReader.readAsDataURL(sFile);
-}
 
-function changeInputFil(oEvent){
-  var oInputFile = oEvent.currentTarget,
-      sName = oInputFile.name,
-      aFiles = oInputFile.files,
-      aAllowedTypes = ['png', 'jpg'],
-      imgType;  
-  document.getElementById('preview-'+sName).innerHTML ='';
-  for (var i = 0 ; i < aFiles.length ; i++) {
-    imgType = aFiles[i].name.split('.');
-    imgType = imgType[imgType.length - 1];
-    if(aAllowedTypes.indexOf(imgType) != -1) {
-      createThumbnail(aFiles[i],sName);
-    }
-  }
-  document.getElementById('preview-').innerHTML ='';
-}
+/*
+const deleteWorkButton = document.getElementById("delete-work");
+const imgLaod = document.getElementById("imgLaod");
+const titre = document.getElementById("titre");
+const categorie = document.getElementById("categorie");
+const validImg = document.querySelector(".validImg");
 
-document.addEventListener('DOMContentLoaded',function(){
- var aFileInput = document.forms['myForm'].querySelectorAll('[type=file]');
-  for(var k = 0; k < aFileInput.length;k++){
-    aFileInput[k].addEventListener('change', changeInputFil, false);
-  }
+deleteWorkButton.addEventListener("click", (event) => {
+  deleteWork();
+  console.log("delete img")
 });
-
-validImg.addEventListener("input", validFormImg);
-function validFormImg() {
-  if (imgLaod.value && titre.value && categorie.value) {
-    validImg.removeAttribute("disabled");
-    validImg.classList.add("disabled");
-  }
-};
 
 const formData = new FormData();
 formData.append("imgLaod");
 formData.append("titre");
 formData.append("categorie");
+*/
